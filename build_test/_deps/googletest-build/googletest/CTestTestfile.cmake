5abbb699a3a4670628afad54a54e384fe016e8ef
mugonmuydesk <mugonmuydesk@users.noreply.github.com>
2025-09-04 16:09:43 +0100

    Remove build_test/_deps directories completely

--- build_test/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/Mouse2VR-Treadmill-Bridge/build_test/_deps/googletest-src/googletest
-# Build directory: C:/dev/Mouse2VR-Treadmill-Bridge/build_test/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
